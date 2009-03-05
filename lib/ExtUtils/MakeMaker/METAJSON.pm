665bd9cb599b74435ddd9503179cf550ac752af9
Ricardo SIGNES <rjbs@cpan.org>
2009-03-04 22:46:29 -0500

    working, 1/4

--- lib/ExtUtils/MakeMaker/METAJSON.pm
+++ lib/ExtUtils/MakeMaker/METAJSON.pm
@@ -1,16 +1,49 @@
 use strict;
 use warnings;
 
-{
-  package ExtUtils::MakeMaker::METAJSON;
-  our $VERSION = '1.000';
-}
+package ExtUtils::MakeMaker::METAJSON;
+our $VERSION = '1.000';
 
-package MY;
+use ExtUtils::MM_Any;
 use JSON 2;
-sub metafile_file {
+
+=head1 NAME
+
+ExtUtils::MakeMaker::METAJSON - put JSON in your META.yml (with EU::MM)
+
+=head1 SYNOPSIS
+
+In your Makefile.PL:
+
+  use ExtUtils::MakeMaker;
+  eval { require ExtUtils::MakeMaker::METAJSON; };
+
+  WriteMakefile(...);
+
+If EU::MM::METAJSON cannot be loaded (for example, because a user who is
+installing your module does not have it or JSON installed), things will
+continue as usual.  If it can be loaded, any META.yml file produced will
+contain JSON.
+
+=cut
+
+no warnings 'redefine';
+*ExtUtils::MM_Any::metafile_file = sub {
   my ($self, %pairs) = @_;
+
   return JSON->new->ascii(1)->pretty->encode(\%pairs);
-}
+};
+
+=head1 SEE ALSO
+
+L<JSON::CPAN::META>
+
+=head1 AUTHOR AND COPYRIGHT
+
+Copyright (C) 2009, Ricardo Signes, C<rjbs@cpan.org>
+
+This is free software, distributed under the same terms as perl5.
+
+=cut
 
 1;
--- lib/JSON/CPAN/META.pm
+++ lib/JSON/CPAN/META.pm
@@ -7,6 +7,35 @@ our $VERSION = '1.000';
 
 JSON::CPAN::META - JSON is YAML; emit JSON into META.yml
 
+=head1 DESCRIPTION
+
+CPAN distributions contain a file, F<META.yml>, which contains a description of
+the distribution and its contents.  This document is notionally written in
+YAML, a powerful data serialization format.  Perl has long lacked any reliable,
+full implementation of YAML.  Instead, it has numerous incompatible and partial
+implementations.
+
+One of the least complete implementations, YAML::Tiny has been advanced to be
+the standard mechanism for parsing these documents.  This defeats the purpose
+of using a powerful serialization language without the benefit of a very simple
+and easily understood data format.
+
+JSON, unlike YAML, is lacking in features but is extremely easy to parse and
+generate correctly.  Further, JSON documents are valid YAML documents.  Thus, a
+META.yml file may contain JSON without violating the spec or introducing
+the sort of problems you'd expect from a poorly implemented YAML emitter.
+
+JSON-CPAN-META contains plugins to allow distribution-building tools to produce
+META.yml files that contain JSON content.
+
+=head1 SEE ALSO
+
+L<ExtUtils::MakeMaker::JSONMETA>
+
+L<Module::Install::JSONMETA>
+
+L<Module::Build::JSONMETA>
+
 =head1 AUTHOR AND COPYRIGHT
 
 Copyright (C) 2009, Ricardo Signes, C<rjbs@cpan.org>
