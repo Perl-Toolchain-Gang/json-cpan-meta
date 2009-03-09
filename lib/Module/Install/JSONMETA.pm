e8c3f2fbd93b8c3c605c0c06ac43c7474c01ccfa
Ricardo SIGNES <rjbs@cpan.org>
2009-03-09 10:29:53 -0400

    make the M:I implementation work

--- lib/Module/Install/JSONMETA.pm
+++ lib/Module/Install/JSONMETA.pm
@@ -1,14 +1,39 @@
 use strict;
 use warnings;
 package Module::Install::JSONMETA;
-our $VERSION = '1.000';
+use Module::Install::Base;
+
+BEGIN {
+  our @ISA = qw(Module::Install::Base);
+  our $ISCORE  = 1;
+  our $VERSION = '1.000';
+}
 
 =head1 NAME
 
-Module::Install::JSONMETA - (unimplemented) write META.yml with JSON syntax
+Module::Install::JSONMETA - write META.yml with JSON syntax
 
 =cut
 
-die "not yet implemented";
+sub jsonmeta {
+  my ($self) = @_;
+
+  no warnings 'redefine';
+  require Module::Install::Metadata;
+  *Module::Install::Metadata::write = sub {
+    my ($self) = @_;
+    return $self unless $self->is_admin;
+
+    unless (eval { require JSON; JSON->VERSION(2); 1 }) {
+     die "could not load JSON.pm version 2 or better; can't use jsonmeta\n";
+    }
+
+    local *YAML::Tiny::Dump = sub { JSON->new->pretty->ascii->encode(shift) . "\n" };
+    
+    $self->admin->write_meta;
+
+    return $self;
+  };
+}
 
 1;
