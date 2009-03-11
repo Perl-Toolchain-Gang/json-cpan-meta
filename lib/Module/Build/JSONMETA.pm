d94840f467c1dd7f8e31f90655707947d3c7748a
Ricardo SIGNES <rjbs@cpan.org>
2009-03-10 23:49:43 -0400

    make MB plugin work

--- lib/Module/Build/JSONMETA.pm
+++ lib/Module/Build/JSONMETA.pm
@@ -1,14 +1,61 @@
 use strict;
 use warnings;
 package Module::Build::JSONMETA;
-our $VERSION = '1.000';
+BEGIN { our $VERSION = '3.000'; }
 
 =head1 NAME
 
-Module::Build::JSONMETA - (unimplemented) write META.yml with JSON syntax
+Module::Build::JSONMETA - write META.yml with JSON syntax
+
+=head1 SYNOPSIS
+
+This interface may be changed in the future if someone with more Module::Build
+expertise steps forward.
+
+  ----- in Build.PL -----
+
+  use Module::Build;
+  use Module::Build::JSONMETA;
+
+  my $class = Module::Build->subclass(
+    code => Module::Build::JSONMETA->code,
+  );
+
+  my $build = $class->new( ... );
+
+  $build->create_build_script;
 
 =cut
 
-die "not yet implemented";
+my $CODE;
+BEGIN {
+  $CODE = <<'END_CODE';
+sub write_metafile {
+  my ($self) = @_;
+  my $data = {};
+  $self->prepare_metadata($data);
+
+  $data->{generated_by} = GENERATED_BY;
+
+  require JSON;
+  JSON->VERSION(2);
+  my $json = JSON->new->ascii(1)->pretty->encode($data);
+
+  my $metafile = $self->metafile;
+
+  open my $fh, '>', $metafile or die "can't open $metafile for writing: $!";
+  print {$fh} $json           or die "can't print metadata to $metafile: $!";
+  close $fh                   or die "error closing $metafile: $!";
+
+  $self->{wrote_metadata} = 1;
+  $self->_add_to_manifest('MANIFEST', $metafile);
+}
+END_CODE
+
+  my $generated_by = join ' version ', __PACKAGE__, __PACKAGE__->VERSION;
+  $CODE =~ s/GENERATED_BY/q{$generated_by}/;
+}
+
+sub code { $CODE }
 
 1;
